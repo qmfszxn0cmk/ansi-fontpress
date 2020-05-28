d99df7503b0356f1efadc0ac482745851b4f21d8
qmfszxn0cmk <qmfszxn0cmk@gmail.com>
2020-05-27 20:48:01 -0500

    Small organizational changes

--- abcs.py
+++ abcs.py
@@ -1,8 +1,50 @@
-# Uppercase
+class MadA:
+    def __init__(self, case='lower'):
+        canvas = create_canvas()
+        x_height = 8
+        
+        if case == 'lower':
+        # change index (0, 2, 6)    
+                pass
+        if case != 'upper':
+            print(unfinished)
 
-# Lowercase
-mad_a =
+"""
 mad_d =
 mad_e =
 mad_s =
 mad_n =
+"""
+class Mad8:
+    def __init__(self, h, x_h, width, weight):
+        pass
+
+def unfinished():
+    return "Sorry, this feature hasn't been completed yet."
+
+def create_canvas(fill='$', width=4, weight=2, letter_height=9, drip_height=2):
+    canvas = []
+    f = fill * weight
+    counter = ' ' * width
+    frame = f + counter + f
+    drip = '.' * len(frame)
+
+    for i in range(letter_height):
+        
+        if i == 0: # top cap
+            canvas.append(frame.replace(' ', '"'))
+            continue
+        if i == letter_height - 1: # bottom cap (above drips)
+            canvas.append(frame.replace(' ', ','))
+            continue
+        canvas.append(frame)
+
+    for i in range(drip_height):
+        canvas.append(drip)
+        
+    return canvas
+
+print('\n')
+blank_1 = create_canvas()
+for frame in blank_1:
+    print(frame)
--- frameworks.py
+++ frameworks.py
@@ -12,3 +12,13 @@ lightning = [" ,           ,     ,       ,   ,       ,   ",
              "   `X`  X,X'   `X'   `X,X'   `   `X,X'   ` ",
              "         `             `           `       "]
 
+madness_elements = {
+       'corner_u_l':',d',
+       'corner_u_r':'b,',
+       'corner_l_l':'`Y',
+       'corner_l_r':"Y'",
+       'cross_u':'"',
+       'cross_lower':',',
+       'negative':' '
+}
+
